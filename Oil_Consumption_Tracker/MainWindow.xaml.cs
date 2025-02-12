17e902eaffc65d171549181f01c3ba9529891e9d
AarniJunkkala <amk1005850@student.hamk.fi>
2025-02-12 19:34:28 +0200

    Base to build project on.
    WPF files, basic classes.

---
+++ Oil_Consumption_Tracker/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oil_Consumption_Tracker
+{
+    class Auto
+    {
+        string rekisteriNumero = "";
+        List<Mittaus> mittaukset = new List<Mittaus>();
+    }
+
+    class Mittaus
+    {
+        double oljynMaara = 0;
+        DateTime paivays;
+
+        Mittaus(double oljynMaara = 0)
+        {
+            this.oljynMaara = oljynMaara;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        List<Auto> autot = new List<Auto>();
+        public MainWindow()
+        {
+            deserialize();
+            InitializeComponent();
+        }
+
+        void deserialize()
+        {
+
+        }
+
+        void serialize()
+        {
+
+        }
+    }
+}
