af2162274bc2a30fc999036e88f8743ae0cf9a2a
TuomasVaahtoluoto <tuomas.vaahtoluoto@student.hamk.fi>
2025-02-13 14:15:04 +0200

    Pääikkunan eka hyvin raffi versio, joka kuitenkin näyttää jo sampledataa
    Tein pääikkunaan perusleiskan, sekä DataGridin, joka hakee pääohjelmassa luotua sampledataa. Näyttää vielä ihan paskalta, mutta dataa näkyy. Muutin samalla luokkia sillai että sain sampledatan toimimaan, eli lisäsin public-arvoja, settereitä ja gettereitä jne.

--- Oil_Consumption_Tracker/MainWindow.xaml.cs
+++ Oil_Consumption_Tracker/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -15,30 +16,53 @@ using System.Windows.Shapes;
 
 namespace Oil_Consumption_Tracker
 {
-    class Auto
+    public class Auto
     {
-        string rekisteriNumero = "";
-        List<Mittaus> mittaukset = new List<Mittaus>();
+        public string rekisteriNumero { get; set; }
+        public List<Mittaus> mittaukset { get; set; } = new List<Mittaus>();
+
+        public Auto(string rekisteriNumero)
+        {
+            this.rekisteriNumero = rekisteriNumero;
+        }
     }
 
-    class Mittaus
+    public class Mittaus
     {
-        double oljynMaara = 0;
-        DateTime paivays;
+        public double oljynMaara { get; set; }
+        public DateTime paivays { get; set; }
+        public Auto auto { get; set; }
 
-        Mittaus(double oljynMaara = 0)
+        public Mittaus(double oljynMaara, Auto auto)
         {
             this.oljynMaara = oljynMaara;
+            this.auto = auto;
+            this.paivays = DateTime.Now;
         }
     }
 
     public partial class MainWindow : Window
     {
-        List<Auto> autot = new List<Auto>();
+
+        public List<Auto> autot { get; set; } = new List<Auto>();
+        
         public MainWindow()
         {
+            DataContext = this;
+
             deserialize();
             InitializeComponent();
+
+            Auto auto1 = new Auto("KJE-123");
+            autot.Add(auto1);
+            auto1.mittaukset.Add(new Mittaus(5.3, auto1));
+            auto1.mittaukset.Add(new Mittaus(4.7, auto1));
+            auto1.mittaukset.Add(new Mittaus(3.9, auto1));
+
+            Auto auto2 = new Auto("FIS-344");
+            autot.Add(auto2);
+            auto2.mittaukset.Add(new Mittaus(10.4, auto2));
+            auto2.mittaukset.Add(new Mittaus(8.2, auto2));
         }
 
         void deserialize()
