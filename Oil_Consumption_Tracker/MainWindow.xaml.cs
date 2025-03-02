a920deb27a84ee3f8f97312ff6bcea2f42beba72
Aarni Junkkala <amk1005850@student.hamk.fi>
2025-03-02 20:42:08 +0200

    Update

--- Oil_Consumption_Tracker/MainWindow.xaml.cs
+++ Oil_Consumption_Tracker/MainWindow.xaml.cs
@@ -16,69 +16,87 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.IO;
 using System.Text.Json;
+using System.Collections.ObjectModel;
 
 namespace Oil_Consumption_Tracker
 {
     public class Car
     {
-        public string registerNumber { get; set; }
-        public List<Measurement> measurements { get; set; } = new List<Measurement>();
+        public string registerNumber { get; set; } = "";
+        public string manufacturer { get; set; } = "";
+        public string motorCode { get; set; } = "";
+        //ObservableCollection on sama kuin lista, mutta gridin näyttäminen toimii samaan aikaan kun muuttaa sisältöä.
+        public ObservableCollection<Measurement> measurements { get; set; } = new ObservableCollection<Measurement>();
 
-        public Car(string registerNumber)
+        public Car(string registerNumber, string manufacture = "No manufacturer found", string motorCode = "No motorcode found")
         {
             this.registerNumber = registerNumber;
+            this.manufacturer = manufacture;
+            this.motorCode = motorCode;
         }
+        public Car() { }
     }
 
     public class Measurement
     {
-        public double oilAmount { get; set; }
-        public DateTime date { get; set; }
-        public Car car { get; set; }
+        public double oilAmount { get; set; } = 0;
+        public DateTime date { get; set; } = DateTime.Now;
 
-        public Measurement(double oljynMaara, Car Car)
+        public Measurement(double oilAmount)
         {
-            this.oilAmount = oljynMaara;
-            this.car = Car;
+            this.oilAmount = oilAmount;
             this.date = DateTime.Now;
         }
+
+        public Measurement() { }
     }
 
     public partial class MainWindow : Window
     {
 
-        public List<Car> cars { get; set; } = new List<Car>();
+        public ObservableCollection<Car> cars { get; set; } = new ObservableCollection<Car>();
         public Grid currentWindow; //The window that is currently visible.
         public double graphMaxHeight = 15; 
 
         public MainWindow()
         {
-            Deserialize();
             InitializeComponent();
             currentWindow = mainMenuWindow;
 
             DataContext = this;
-
-            TestiArvot();
+            //TestiArvot();
+            Deserialize();
         }
 
         void TestiArvot()
         {
-            Car car1 = new Car("KJE-123");
-            car1.measurements.Add(new Measurement(5.3, car1));
-            car1.measurements.Add(new Measurement(4.7, car1));
-            car1.measurements.Add(new Measurement(3.9, car1));
-            cars.Add(car1);
+            cars.Add(new Car("KJE-123"));
+            cars[0].measurements.Add(new Measurement(5.3));
+            cars[0].measurements.Add(new Measurement(4.7));
+            cars[0].measurements.Add(new Measurement(3.9));
 
-            Car car2 = new Car("FIS-344");
-            car2.measurements.Add(new Measurement(10.4, car2));
-            car2.measurements.Add(new Measurement(8.2, car2));
-            car2.measurements.Add(new Measurement(2.1, car2));
-            car2.measurements.Add(new Measurement(4.2, car2));
-            car2.measurements.Add(new Measurement(0.2, car2));
-            cars.Add(car2);
+            cars.Add(new Car("FIS-344"));
+            cars[1].measurements.Add(new Measurement(10.4));
+            cars[1].measurements.Add(new Measurement(8.2));
+            cars[1].measurements.Add(new Measurement(2.1));
+            cars[1].measurements.Add(new Measurement(4.2));
+            cars[1].measurements.Add(new Measurement(0.2));
+        }
 
-            ShowGrahp(car2);
+        void ValidateRegisternumber(object sender, TextChangedEventArgs e)
+        {
+            saveCarButton.IsEnabled = !(addRegisterNumber.Text == "");
+        }
+
+        void AddCar(object sender, RoutedEventArgs e)
+        {
+            if(addRegisterNumber.Text == "")
+            {
+                return;
+            }
+            Application.Current.Dispatcher.Invoke(() => cars.Add(new Car(addRegisterNumber.Text, addManufacturer.Text, addMotorcode.Text)));
+            Serialize();
+            OpenWindow("mainMenuWindow");
         }
 
         void Deserialize()
@@ -87,7 +105,8 @@ namespace Oil_Consumption_Tracker
             if (File.Exists(filePath))
             {
                 string jsonString = File.ReadAllText(filePath);
-                cars = JsonSerializer.Deserialize<List<Car>>(jsonString) ?? new List<Car>();
+                var options = new JsonSerializerOptions { IncludeFields = true };
+                cars = JsonSerializer.Deserialize<ObservableCollection<Car>>(jsonString, options) ?? new ObservableCollection<Car>();
             }
         }
 
